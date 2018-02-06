2f47078996f93083a88ec619f6eb3d23f5ed6934
Adam Bugaj <adampeter.bugaj@gmail.com>
2018-02-06 20:30:26 +0100

    Setup production version

--- server/server.js
+++ server/server.js
@@ -7,8 +7,6 @@ const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-  res.sendfile(path.join(publicPath, 'index.html'));
+  res.send(path.join(publicPath, 'index.html'));
 });
-app.listen(port, () => {
-  console.log('server is up!');
-});
+app.listen(port);
